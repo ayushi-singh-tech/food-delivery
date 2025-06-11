3397ab9fd312a4f06f7958e0bfb263af65270ef0
ayushi-singh-tech <singhayu360@gmail.com>
2025-06-11 17:59:20 +0530

    folder structure created

---
+++ src/AppRouter.tsx
@@ -0,0 +1,25 @@
+import { IonRouterOutlet } from '@ionic/react'
+import { IonReactRouter } from '@ionic/react-router'
+import React from 'react'
+import { Redirect, Route } from 'react-router-dom'
+import Home from './screens/HomeScreen'
+
+const AppRouter = () => {
+  return (
+    <>
+      <IonReactRouter>
+      <IonRouterOutlet>
+        <Route exact path="/home">
+          <Home />
+        </Route>
+        <Route exact path="/">
+          <Redirect to="/home" />
+        </Route>
+      </IonRouterOutlet>
+    </IonReactRouter>
+      
+    </>
+  )
+}
+
+export default AppRouter
